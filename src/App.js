cb5027324126dbd457232643db988f3be64da10d
ozaki <fs65ugumori@yahoo.co.jp>
2020-01-01 23:01:29 +0900

    create page transition animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -19,18 +19,48 @@ const Item = styled.div`
 
 function App() {
   const [baseIndex, setBaseIndex] = useState(1);
-  const props = useSpring({
+  const [direction, setDirection] = useState(null);
+
+  const translateX =
+    direction === 'PREV' ? '-50px' : direction === 'NEXT' ? '50px' : 0;
+
+  const transitions = useTransition(baseIndex, item => item, {
     config: { duration: 100 },
-    to: { transform: 'translateX(0)', opacity: 1 },
-    from: { transform: 'translateX(50px)', opacity: 0 },
+    from: {
+      transform: `translateX(${translateX})`,
+      opacity: 0,
+    },
+    enter: { transform: 'translateX(0)', opacity: 1 },
+    leave: { display: 'none' },
   });
+
+  const onClickPrev = () => {
+    setBaseIndex(prevIndex => prevIndex - 7);
+    setDirection('PREV');
+  };
+
+  const onClickNext = () => {
+    setBaseIndex(prevIndex => prevIndex + 7);
+    setDirection('NEXT');
+  };
+
   return (
     <Container>
-      <Items style={props}>
-        {[...Array(baseIndex + 6)].map((_, i) => (
-          <Item key={i}>{i}</Item>
-        ))}
-      </Items>
+      <button onClick={onClickPrev}>
+        <span>⬅️</span>
+        前へ
+      </button>
+      <button onClick={onClickNext}>
+        次へ
+        <span>➡️</span>
+      </button>
+      {transitions.map(({ item, key, props }) => (
+        <Items style={props} key={key}>
+          {[...Array(7)].map((_, i) => (
+            <Item key={item + i}>{item + i}</Item>
+          ))}
+        </Items>
+      ))}
     </Container>
   );
 }
