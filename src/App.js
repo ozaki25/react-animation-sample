47650d6b46b0136623c666cd87986a14cc5ab931
ozaki <fs65ugumori@yahoo.co.jp>
2020-01-01 21:54:25 +0900

    calendar sample

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 15px;
+`;
+
+const Items = styled(animated.div)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Item = styled.div`
+  border: solid 1px #bbb;
+  width: 100%;
+  height: 100px;
+`;
 
 function App() {
+  const [baseIndex, setBaseIndex] = useState(1);
   const props = useSpring({
+    config: { duration: 100 },
     to: { transform: 'translateX(0)', opacity: 1 },
-    from: { transform: 'translateX(100%)', opacity: 0 },
+    from: { transform: 'translateX(50px)', opacity: 0 },
   });
   return (
-    <div>
-      <animated.h1 style={props}>Hello</animated.h1>
-    </div>
+    <Container>
+      <Items style={props}>
+        {[...Array(baseIndex + 6)].map((_, i) => (
+          <Item key={i}>{i}</Item>
+        ))}
+      </Items>
+    </Container>
   );
 }
 
